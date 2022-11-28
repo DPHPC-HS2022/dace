fbe58713fc8faa6fcc1b4528a01dfcaf845f1156
Navaneeth-KunhiPurayil <nkunhi@student.ethz.ch>
2022-11-28 23:30:09 +0100

    edited codegen.py to generated C code with benchmark parameters

--- benchmarking/README.md
+++ benchmarking/README.md
@@ -1,11 +1,19 @@
-#Steps to run benchmark
+# Setup
 
-1- Setup npbench benchmark by running 
+Setup npbench benchmark by running 
 ```
 bash setup_benchmark.sh
 ```
+# Run benchmarks 
 
-2- Run the benchmark (specify the ./run-id/ directory if required, default is ./run1/)
+Run the benchmark (specify the ./run-id/ directory if required, default is ./run1/)
+Reads the dace benchmark programs from benchmarks.txt, generates code for openmp tasks and parallel for, compiles and runs the generated C code.
 ```
 python code_generator.py --id 1
 ```
+
+# Adding benchmarks
+
+benchmarks.txt contains the benchmarks to be run and the path to the benchmark-folder
+1. Create a json file containing, parameters of the module name (name of the python file), function name (name of the function within the file), relative path (with respect to benchmark-folder),...
+2. Add the path of the json file to benchmarks.txt
--- benchmarking/code_generator.py
+++ benchmarking/code_generator.py
@@ -5,13 +5,14 @@ from dace.codegen.codegen import *
 from dace.codegen.compiler import *
 import glob, re, json
 import os, importlib
+import time
 
-def generate_benchmark_code(func, output_path, config):
+def generate_benchmark_code(func, output_path, config, params_dict):
     # Generate SDFG for the function
     sdfg = func.to_sdfg()
          
     # Generate code from the SDFG
-    code_objects = generate_code(sdfg) # List of code objects  
+    code_objects = generate_code(sdfg,params_dict) # List of code objects  
     generate_program_folder(sdfg, code_objects, output_path, config=config)
     lib_file = configure_and_compile(output_path, program_name="test", output_stream=None)
 
@@ -20,35 +21,46 @@ def execute_benchmark_code(output_path):
     path = os.getcwd()
     os.chdir(output_path + '/build')
     os.system('gcc -I '+ path +'/../dace/runtime/include/ -o bin ../sample/*.cpp -L . -ltest')
-    os.system('LD_LIBRARY_PATH=.:$LD_LIBRARY_PATH ./bin')
+    avg = 0
+    N = 10
+    for i in range(N):
+        st = time.process_time()
+        os.system('LD_LIBRARY_PATH=.:$LD_LIBRARY_PATH ./bin')
+        et = time.process_time()
+        avg += et-st
+    print(output_path," Average exec time:",str(avg/N))
     os.chdir(path)
 
 def generate(id):
     config = Config()
 
     # Read every function name in npbench
-    benchmark_list = glob.glob("./npbench/bench_info/*")
-    for benchmark_info in benchmark_list:
-   
+    #benchmark_list = glob.glob("./npbench/bench_info/*")
+    benchmark_list_file = open("benchmarks.txt","r")
+
+    #for benchmark_info in benchmark_list:
+    for benchmark_info in benchmark_list_file:
             # Extract path from json
         
-            jsonfile = open(benchmark_info,"r")
+            jsonfile = open(benchmark_info.split(",")[0],"r")
             benchmark_data = json.load(jsonfile)
             jsonfile.close()
 
             print("BENCHMARK:", benchmark_info)
-            #print(benchmark_data,"\n\n")
 
+            # Required json parameters for a benchmark
             rel_path = benchmark_data['benchmark']['relative_path']
+            module_name = benchmark_data['benchmark']['module_name']
             func_name = benchmark_data['benchmark']['func_name']
+            params_dict = benchmark_data['benchmark']['parameters']['S']
 
             # Import module
-            path_to_import = "benchmarking.npbench.npbench.benchmarks"
+            path_to_import = benchmark_info.split(",")[1]
             rel_path_list = rel_path.split('/')
             for i in range(len(rel_path_list)):
                 path_to_import = path_to_import + "." + rel_path_list[i]
                 if (i == (len(rel_path_list)-1)):
-                    path_to_import = path_to_import + "." + benchmark_data['benchmark']['module_name'] + "_dace"
+                    path_to_import = path_to_import + "." + module_name + "_dace"
 
             print(path_to_import)
             module = importlib.import_module(path_to_import)
@@ -62,7 +74,7 @@ def generate(id):
                 output_path = "run" + str(id) + "/build-" + rel_path + "-" + func_name + "-useTasks=" + str(omp_use_task)
                 
                 try:             
-                    generate_benchmark_code(func, output_path, config)
+                    generate_benchmark_code(func, output_path, config, params_dict)
                     execute_benchmark_code(output_path)
                 #except (dace.codegen.exceptions.CompilerConfigurationError, KeyError):
                 except Exception as e:
